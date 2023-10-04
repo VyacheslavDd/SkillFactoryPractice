5bbf40036cd42e4f8d11f820d1dda8075eabe214
VyacheslavDd <Sonic.x.exe@yandex.ru>
2023-10-04 23:47:01 +0500

    module3 final task done

--- SkillFactory/Main.cs
+++ SkillFactory/Main.cs
@@ -1,5 +1,6 @@
 ﻿using SkillFactory.Programs;
 using SkillFactory.Programs.Module_1;
 using SkillFactory.Programs.Module_2;
+using SkillFactory.Programs.Module_3;
 
-Task_2_6_2.SortEvent();
+Task_3_5_1.InterfaceCalculatorShowCase();
---
+++ SkillFactory/Programs/Module_3/Task 3_5_1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory.Programs.Module_3
+{
+    internal class Task_3_5_1
+    {
+        private interface ICalculator
+        {
+            public double Sum(double a, double b);
+        }
+
+        private interface ILogger
+        {
+            public void Success(string message);
+            public void Error(string message);
+        }
+
+        private class Calculator : ICalculator, ILogger
+        {
+            public void Error(string message)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+
+            public void Success(string message)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine(message);
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+
+            public double Sum(double a, double b)
+            {
+                return a + b;
+            }
+        }
+
+        public static void InterfaceCalculatorShowCase()
+        {
+            var calculator = new Calculator();
+            try
+            {
+                var firstNumber = double.Parse(Console.ReadLine());
+                var secondNumber = double.Parse(Console.ReadLine());
+                calculator.Success(calculator.Sum(firstNumber, secondNumber).ToString());
+            }
+            catch (FormatException ex)
+            {
+                calculator.Error(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Программа завершена!");
+            }
+        }
+    }
+}
